daff5e6a77c3c2fd6d387694491a5c2d047ec7c3
ssergo99 <95188760+ssergo99@users.noreply.github.com>
2023-01-15 23:14:17 +0300

    DZ4

--- library/app/views.py
+++ library/app/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework import mixins
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
